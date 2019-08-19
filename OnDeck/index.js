1461585726ec165bc442aa44f11879bd417bb895
Kevin Poston <kposton@qualtrax.com>
2019-08-19 08:15:51 -0400

    adding more stuff, need to refactor bad

--- OnDeck/index.js
+++ OnDeck/index.js
@@ -1,14 +1,33 @@
 const fetch = require('node-fetch');
+const moment = require('moment');
+
+const getQualtraxRepoLink = function(gitHubIssueNumber) {
+  return `https://github.com/Qualtrax/Qualtrax/issues/${gitHubIssueNumber}`;
+}
+
+const getQualtraxWebRepoLink = function(gitHubIssueNumber) {
+  return `https://github.com/Qualtrax/qualtrax-web/issues/${gitHubIssueNumber}`;
+}
+
+const parseLink = function(gitHubCommitMessage) {
+  const issueNumberRegEx = /\[(.*?)\]/;
+  const issueHeading = gitHubCommitMessage.match(issueNumberRegEx)[1];
+  const isWeb = issueHeading.startsWith('ghweb');
+
+  if (isWeb)
+    return getQualtraxWebRepoLink(issueHeading.replace('ghweb', ''));
+  else
+    return getQualtraxRepoLink(issueHeading.replace('gh', ''));
+}
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    let reply = "HI THERE";
     let releaseResponse = '';
     const owner = "Qualtrax";
     const name = "Qualtrax";
-    let query = `query GetRelease($owner: String!, $name: String!){
+    let query = `query GetReleases($owner: String!, $name: String!){
       repository(owner: $owner, name: $name) {
-        refs(last: 1, refPrefix: "refs/tags/") {
+        refs(last: 10, refPrefix: "refs/tags/") {
           tags: edges {
             node {
               tag: target {
@@ -42,9 +61,30 @@ module.exports = async function (context, req) {
         })
     })  
     .then(r => r.json())
-    .then(data => reply = data);
+    .then(data => releaseResponse = data);
 
-    const date = reply.data.repository.refs.tags[0].node.tag.commit.committedDate;
+    const responseTags = releaseResponse.data.repository.refs.tags;
+    const isMajor = function(tagName) {
+      return tagName.split('.')[3] == '0';
+    }
+
+    const tags = responseTags.map(element => {
+      return {
+        name: element.node.tag.name,
+        committedDate: element.node.tag.commit.committedDate,
+        isMajor: isMajor(element.node.tag.name)
+      };
+    });
+
+    const majorTags = tags.filter(tag => tag.isMajor);
+
+    const sortedTags = majorTags.sort(function(a, b) {
+      return a.committedDate < b.committedDate;
+    });
+
+    let tag = sortedTags[0];
+    let date = moment(tag.committedDate).add(1, 'seconds');
+
     let commitResponse = '';
     query = `query GetCommits($owner: String!, $name: String!, $date: GitTimestamp!){
       repository(owner: $owner, name: $name) {
@@ -87,26 +127,110 @@ module.exports = async function (context, req) {
     .then(r => r.json())
     .then(data => commitResponse = data);
 
-    const commits = commitResponse.data.repository.defaultBranchRef.target.history.commits;
-    const gitHubIssues = commits.filter(node => node.commit.messageHeadline.startsWith('['));
-    const bulletedList = gitHubIssues.map(issue => {
-      return `• ${ issue.commit.messageHeadline }`;
-    });
+    let commits = commitResponse.data.repository.defaultBranchRef.target.history.commits;
+    let gitHubIssues = commits.filter(node => node.commit.messageHeadline.startsWith('['));
+    gitHubIssues = gitHubIssues.map(issue => {
+      return {
+        message: issue.commit.messageHeadline,
+        link: parseLink(issue.commit.messageHeadline)
+      }
+    })
+    let responses = [];
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            body: {
-              "text": bulletedList.join(' \n ')
-            },
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
+    if (gitHubIssues.length) {
+      const bulletedList = gitHubIssues.map(issue => {
+        return `:moneybag: <${issue.link}|${ issue.message }>`;
+      });
+      const moneyBags = gitHubIssues.map(issue => ":moneybag:");
+      bulletedList.unshift(`:qtrax: *Commits since \`${tag.name}\`:*`);
+      bulletedList.push(":koala: *Should we be shipping this value?!* :koala:");
+      bulletedList.push('');
+      bulletedList.push('');
+      bulletedList.push(moneyBags);
+      responses = bulletedList;
     }
     else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+      date = moment(sortedTags[1].committedDate).add(1, 'seconds');
+      
+      commitResponse = '';
+      query = `query GetCommits($owner: String!, $name: String!, $date: GitTimestamp!){
+        repository(owner: $owner, name: $name) {
+          defaultBranchRef {
+            target {
+              ... on Commit {
+                history(since: $date) {
+                  totalCount
+                  commits: edges {
+                    commit: node {
+                      ... on Commit {
+                        committedDate
+                        messageHeadline
+                      }
+                    }
+                  }
+                  pageInfo {
+                    endCursor
+                    hasNextPage
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`;
+
+      await fetch('https://api.github.com/graphql', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              'Authorization': `Bearer ${process.env['GITHUB_AUTH_TOKEN']}`
+          },
+          body: JSON.stringify({
+            query,
+            variables: { owner, name, date }
+          })
+      })  
+      .then(r => r.json())
+      .then(data => commitResponse = data);
+
+      commits = commitResponse.data.repository.defaultBranchRef.target.history.commits;
+      gitHubIssues = commits.filter(node => node.commit.messageHeadline.startsWith('['));
+      gitHubIssues = gitHubIssues.map(issue => {
+        return {
+          message: issue.commit.messageHeadline,
+          link: parseLink(issue.commit.messageHeadline)
+        }
+      })
+
+      const deliveredIssues = gitHubIssues.map(issue => {
+        return `:moneybag: <${issue.link}|${ issue.message }>`;
+      });
+
+      responses = deliveredIssues;
+      responses.unshift("*Here's what was included:*");
+      responses.unshift('');
+      responses.unshift(`:koala: *Nothing on deck because we just tagged the \`${tag.name}\` release ${moment(tag.committedDate).fromNow()}!* :confetti_ball:`)
+    }
+
+    if (responses.length) {
+      context.res = {
+        body: {
+          "text": responses.join(' \n ')
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      };
+    }
+    else {
+      context.res = {
+        body: {
+          "text": "Didn't find anything..."
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      };
     }
 };
